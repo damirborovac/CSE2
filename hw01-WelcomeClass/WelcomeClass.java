221e1fe1b27b7391b20a411b1260e3779be4013f
Damir Borovac <damir.borovac@gmail.com>
2014-09-03 02:24:51 +0000

    WelcomeClass homework

---
+++ hw01-WelcomeClass/WelcomeClass.java
@@ -0,0 +1,25 @@
+// Damir Borovac 
+// HW #01
+// Welcome Class Java Program
+//
+
+// Defining a class
+public class WelcomeClass {
+    
+// Adding main method
+  public static void main(String[] args ) {
+        
+     System.out.println("  -----------"); // Code for the text 
+     System.out.println("  | WELCOME |"); // required to be 
+     System.out.println("  -----------"); // outputted by the 
+     System.out.println("  ^  ^  ^  ^  ^  ^"); // program. 
+     System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+     System.out.println("|-D--A--B--3--1--5-|");
+     System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+     System.out.println("  v  v  v  v  v  v");
+     System.out.println("In August I visited the Mercedes-Benz museum.");
+     System.out.println("The museum's smoke dispelling system is a man-made tornado.");
+        
+        
+    }
+}
